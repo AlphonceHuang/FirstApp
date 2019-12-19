3dc0dd37ea039a6e3bbabcdb7661071f737d6481
AlphonceHuang <58068802+AlphonceHuang@users.noreply.github.com>
2019-12-19 15:55:50 +0800

    add show result button

--- app/src/main/java/com/example/myapplication/CheckBoxRadioBiuttonActivity.java
+++ app/src/main/java/com/example/myapplication/CheckBoxRadioBiuttonActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
@@ -13,21 +15,24 @@ import static com.example.myapplication.Util.showToastIns;
 public class CheckBoxRadioBiuttonActivity extends AppCompatActivity {
 
     private RadioButton rb1, rb2, rb3, rb4, rb5, rb6, rb7;
+    private CheckBox city1, city2, city3, city4, city5, city6, city7, city8, city9, city10;
+    private static final String TAG="Alan";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_box_radio_biutton);
 
-        CheckBox city1 = findViewById(R.id.CB_city1);
-        CheckBox city2 = findViewById(R.id.CB_city2);
-        CheckBox city3 = findViewById(R.id.CB_city3);
-        CheckBox city4 = findViewById(R.id.CB_city4);
-        CheckBox city5 = findViewById(R.id.CB_city5);
-        CheckBox city6 = findViewById(R.id.CB_city6);
-        CheckBox city7 = findViewById(R.id.CB_city7);
-        CheckBox city8 = findViewById(R.id.CB_city8);
-        CheckBox city9 = findViewById(R.id.CB_city9);
-        CheckBox city10 = findViewById(R.id.CB_city10);
+        city1 = findViewById(R.id.CB_city1);
+        city2 = findViewById(R.id.CB_city2);
+        city3 = findViewById(R.id.CB_city3);
+        city4 = findViewById(R.id.CB_city4);
+        city5 = findViewById(R.id.CB_city5);
+        city6 = findViewById(R.id.CB_city6);
+        city7 = findViewById(R.id.CB_city7);
+        city8 = findViewById(R.id.CB_city8);
+        city9 = findViewById(R.id.CB_city9);
+        city10 = findViewById(R.id.CB_city10);
         city1.setOnCheckedChangeListener(mCheckBox);
         city2.setOnCheckedChangeListener(mCheckBox);
         city3.setOnCheckedChangeListener(mCheckBox);
@@ -48,8 +53,19 @@ public class CheckBoxRadioBiuttonActivity extends AppCompatActivity {
         rb7=findViewById(R.id.radioButton7);
         RadioGroup rg=findViewById(R.id.RadioGroup);
         rg.setOnCheckedChangeListener(mRadioButton);
+
+        Button result = findViewById(R.id.CB_Result);
+        result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showResult();
+            }
+        });
     }
 
+    //---------------------------------------------------------------------
+    // check box event
+    //---------------------------------------------------------------------
     private CompoundButton.OnCheckedChangeListener mCheckBox = new CompoundButton.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -61,6 +77,9 @@ public class CheckBoxRadioBiuttonActivity extends AppCompatActivity {
         }
     };
 
+    //---------------------------------------------------------------------
+    // Radio button event
+    //---------------------------------------------------------------------
     private RadioGroup.OnCheckedChangeListener mRadioButton = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
@@ -91,4 +110,32 @@ public class CheckBoxRadioBiuttonActivity extends AppCompatActivity {
             showToastIns(getApplicationContext(), "選了:"+food, Toast.LENGTH_LONG);
         }
     };
+
+    private void showResult(){
+        StringBuilder selectedData = new StringBuilder();
+        int selectedCount = 0;
+
+        boolean[] itemselect=new boolean[]{false, false, false, false, false,
+                                           false, false, false, false, false};
+        itemselect[0]=city1.isChecked();
+        itemselect[1]=city2.isChecked();
+        itemselect[2]=city3.isChecked();
+        itemselect[3]=city4.isChecked();
+        itemselect[4]=city5.isChecked();
+        itemselect[5]=city6.isChecked();
+        itemselect[6]=city7.isChecked();
+        itemselect[7]=city8.isChecked();
+        itemselect[8]=city9.isChecked();
+        itemselect[9]=city10.isChecked();
+
+        for (int i=0; i<10; i++) {
+            //Log.w(TAG, "item "+i+" :"+itemselect[i]);
+
+            if (itemselect[i]) {
+                selectedData.append(getResources().getStringArray(R.array.array_Places)[i]).append(",");
+                selectedCount++;
+            }
+        }
+        showToastIns(this, "總共選取"+selectedCount+"個項目..."+selectedData, Toast.LENGTH_LONG);
+    }
 }
