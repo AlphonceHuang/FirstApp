41a521de0568f32e31f5ee28d1a16ba39ece6bd0
AlphonceHuang <58068802+AlphonceHuang@users.noreply.github.com>
2020-01-02 12:03:35 +0800

    splash add input name & password

--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -3,17 +3,26 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static com.example.myapplication.Util.showToastIns;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final String TAG="Alan";
+    private EditText name, password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_splash);
-
+/*
         Thread myThread=new Thread(){//创建子线程
             @Override
             public void run() {
@@ -28,5 +37,26 @@ public class SplashActivity extends AppCompatActivity {
             }
         };
         myThread.start();
+ */
+        name=findViewById(R.id.editNameText);
+        password=findViewById(R.id.editPWDText);
+    }
+
+    boolean checkPassword(){
+        String inputName=name.getText().toString();
+        String inputPassword=password.getText().toString();
+
+        Log.w(TAG, "Name:"+inputName+"\n"+"Password:"+inputPassword);
+        return true;
+    }
+
+    public void entermain(View view){
+        if (checkPassword()) {
+            Intent it = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(it);
+            finish();
+        }else{
+            showToastIns(getApplicationContext(), getString(R.string.cal_fail), Toast.LENGTH_LONG);
+        }
     }
 }
